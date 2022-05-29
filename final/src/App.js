c94f29b4c5e209667ecf3e0b574d3f01f60f37ba
iamrosada <luisrosada2018@gmail.com>
2022-05-29 12:39:47 +0300

    the component App is our main component

---
+++ final/src/App.js
@@ -0,0 +1,13 @@
+import { Container } from "./components/container/container";
+import { Header } from "./components/header/header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Container></Container>
+    </>
+  );
+}
+
+export default App;
