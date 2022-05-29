27e827191f9aa0fc62c70478bc918ea3e7737e4a
iamrosada <luisrosada2018@gmail.com>
2022-05-29 12:36:24 +0300

    header components

---
+++ final/src/components/header/header.js
@@ -0,0 +1,11 @@
+import "./header.styles.css";
+
+export const Header = () => {
+  return (
+    <header id="header">
+      <div id="containerInput">
+        <input placeholder="search all students" type="text" />
+      </div>
+    </header>
+  );
+};
