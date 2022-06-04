3d66d067a480799e4d0d1f1eb91ff73fc6da01d9
iamrosada <luisrosada2018@gmail.com>
2022-06-04 12:08:40 +0300

    feature: updated the component app and added context api

--- final/src/App.js
+++ final/src/App.js
@@ -1,12 +1,18 @@
+import { useState } from "react";
 import { Container } from "./components/container/container";
 import { Header } from "./components/header/header";
+import { UserContext } from "./context/context.api";
 
 function App() {
+  const [search, setSearch] = useState("");
+  const [openModalCreate, setOpenModalCreate] = useState(false);
   return (
-    <>
+    <UserContext.Provider
+      value={[search, setSearch, openModalCreate, setOpenModalCreate]}
+    >
       <Header />
       <Container></Container>
-    </>
+    </UserContext.Provider>
   );
 }
 
